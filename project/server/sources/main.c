1b6f89c19ea8514772046fd3a59b2793e99944d6
Raphael De Freitas <raphythegeek@gmail.com>
2014-05-12 14:11:40 +0200

    Foundation again...

---
+++ project/server/sources/main.c
@@ -0,0 +1,16 @@
+/*
+** main.c for zappy in /home/raphy/Epitech/Tech_2/zappy/project/server/sources
+** 
+** Made by raphael defreitas
+** Login   <defrei_r@epitech.net>
+** 
+** Started on  Mon May 12 14:06:41 2014 raphael defreitas
+** Last update Mon May 12 14:07:03 2014 raphael defreitas
+*/
+
+#include	<stdlib.h>
+
+int		main(void)
+{
+  return (EXIT_SUCCESS);
+}
