ff98381682b5cbc04c9fde024c81b561a9256d52
Raphael De Freitas <raphythegeek@gmail.com>
2014-06-27 18:26:28 +0200

    [libzappy] A method to disconnect a specific client (only for server)

---
+++ project/libraries/libzappy/sources/zc_disconnect.c
@@ -0,0 +1,21 @@
+/*
+** zc_disconnect.c for Zappy in /home/raphy/Epitech/Tech_2/zappy/project/libraries/libzappy/sources
+** 
+** Made by raphael defreitas
+** Login   <defrei_r@epitech.net>
+** 
+** Started on  Fri Jun 27 18:08:49 2014 raphael defreitas
+** Last update Fri Jun 27 18:09:26 2014 raphael defreitas
+*/
+
+#include	<stdbool.h>
+#include	<stdlib.h>
+
+#include	"zappy.h"
+
+void		zc_disconnect(t_zc *this)
+{
+  if (this == NULL)
+    return ;
+  this->has_to_disconnect = true;
+}
