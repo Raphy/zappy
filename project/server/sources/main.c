095f0c744172928105c916d03260b52ea0e57f9d
Raphael De Freitas <raphythegeek@gmail.com>
2014-06-27 18:04:03 +0200

    [server] The news hooks have been handled

--- project/server/sources/main.c
+++ project/server/sources/main.c
@@ -5,7 +5,7 @@
 ** Login   <defrei_r@epitech.net>
 ** 
 ** Started on  Mon May 12 14:06:41 2014 raphael defreitas
-** Last update Thu Jun 26 16:24:33 2014 raphael defreitas
+** Last update Fri Jun 27 17:38:00 2014 raphael defreitas
 */
 
 #include	<errno.h>
@@ -28,9 +28,14 @@ static void	kikoo_header(void)
   printf("+---------------------------------+\n");
 }
 
-static void	nc_handler(t_zs *this, t_zc *zc, void *data)
+static void	cc_handler(t_zs *this, t_zc *zc, void *data)
 {
-  printf("hook new client! fd:%d\n", zc->socket->fd);
+  printf("hook client connected! fd:%d\n", zc->socket->fd);
+}
+
+static void	cd_handler(t_zs *this, t_zc *zc, void *data)
+{
+  printf("hook client disconnected! fd:%d\n", zc->socket->fd);
 }
 
 static void	to_handler(t_zs *this, void *data)
@@ -39,11 +44,27 @@ static void	to_handler(t_zs *this, void *data)
   zs_disable_timeout(this);
 }
 
+static void	errno_handler(t_zs *this, int err, const char *msg, void *data)
+{
+  fprintf(stderr, "[ERROR] ");
+  if (err == 0 && msg == NULL)
+    fprintf(stderr, "Unknown error");
+  else if (err == 0 && msg != NULL)
+    fprintf(stderr, "%s", msg);
+  else if (err != 0 && msg == NULL)
+    fprintf(stderr, "%s", strerror(err));
+  else
+    fprintf(stderr, "%s (%s)", strerror(err), msg);
+  fprintf(stderr, "\n");
+}
+
 static void	init_server(t_zs *server)
 {
   zs_set_timeout(server, 1, 500);
   zs_hook_timeout(server, &to_handler, NULL);
-  zs_hook_client_connected(server, &nc_handler, NULL);
+  zs_hook_client_connected(server, &cc_handler, NULL);
+  zs_hook_client_disconnected(server, &cd_handler, NULL);
+  zs_hook_errno(server, &errno_handler, NULL);
 }
 
 int		main(int argc, char **argv)
