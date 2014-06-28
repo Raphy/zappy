3b4a161c0bf7f9b830f5e60d6b6a475ecad6ec8b
Raphael De Freitas <raphythegeek@gmail.com>
2014-06-29 00:57:06 +0200

    [server] remove the timeout after the first handle

--- project/server/sources/handlers/timeout_handler.c
+++ project/server/sources/handlers/timeout_handler.c
@@ -5,15 +5,16 @@
 ** Login   <defrei_r@epitech.net>
 ** 
 ** Started on  Fri Jun 27 19:29:56 2014 raphael defreitas
-** Last update Fri Jun 27 19:33:21 2014 raphael defreitas
+** Last update Sat Jun 28 23:01:20 2014 raphael defreitas
 */
 
 #include	<stdio.h>
 
 #include	"zappy.h"
 
-void		timeout_handler(__attribute__((unused))t_zs *zs,
+void		timeout_handler(t_zs *zs,
 				__attribute__((unused))void *data)
 {
   printf("timeout handler\n");
+  zs_disable_timeout(zs);
 }
