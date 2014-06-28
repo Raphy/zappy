e812822ec8baf10dd829004504f1cdc3343c0f4e
Raphael De Freitas <raphythegeek@gmail.com>
2014-06-28 02:05:32 +0200

    [server] use the data parameter to the handlers

--- project/server/sources/handlers/set_server_handlers.c
+++ project/server/sources/handlers/set_server_handlers.c
@@ -5,7 +5,7 @@
 ** Login   <defrei_r@epitech.net>
 ** 
 ** Started on  Fri Jun 27 19:46:18 2014 raphael defreitas
-** Last update Fri Jun 27 19:49:43 2014 raphael defreitas
+** Last update Sat Jun 28 02:03:22 2014 raphael defreitas
 */
 
 #include	"handlers.h"
@@ -13,10 +13,10 @@
 
 void		set_server_handlers(t_zs *zs, void *data)
 {
-  zs_hook_errno(zs, &errno_handler, NULL);
-  zs_hook_callback(zs, &callback_handler, NULL);
+  zs_hook_errno(zs, &errno_handler, data);
+  zs_hook_callback(zs, &callback_handler, data);
   zs_set_timeout(zs, 2, 500);
-  zs_hook_timeout(zs, &timeout_handler, NULL);
-  zs_hook_client_connected(zs, &client_connected_handler, NULL);
-  zs_hook_client_disconnected(zs, &client_disconnected_handler, NULL);
+  zs_hook_timeout(zs, &timeout_handler, data);
+  zs_hook_client_connected(zs, &client_connected_handler, data);
+  zs_hook_client_disconnected(zs, &client_disconnected_handler, data);
 }
