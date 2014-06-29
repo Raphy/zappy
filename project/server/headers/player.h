312925678040b0820247e96ccae406ee63db5ce3
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-06-29 12:49:58 +0200

    [Server] : Struct Player , inventaire 1 version

---
+++ project/server/headers/player.h
@@ -0,0 +1,35 @@
+/*
+** player.h for zappy in /home/damien/zappy/project/server/headers
+**
+** Made by damien sauvalle
+** Login   <sauval_d@epitech.net>
+**
+** Started on  Sun Jun 29 11:57:43 2014 damien sauvalle
+** Last update Sun Jun 29 12:47:44 2014 damien sauvalle
+*/
+
+#ifndef		PLAYER_H_
+# define	PLAYER_H_
+
+typedef struct	s_stock
+{
+  /*
+  ** compteur de ressource
+  ** Pierre + miam
+  ** unsign int
+  */
+}		t_stock;
+
+typedef struct	s_player
+{
+  /*
+  ** struct inventaire
+  ** ENUM Direction
+  ** coordone x y
+  ** la structure de raphy
+  ** equipe
+  */
+}		t_player;
+
+
+#endif /* !PLAYER_H_ */
