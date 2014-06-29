c90163246c7ecbd6589b831e73d2aebd5ea78da6
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-06-29 19:01:22 +0200

    [Serveur] : inventory c/d_tor

---
+++ project/server/sources/inventory/inventory_dtor.c
@@ -0,0 +1,16 @@
+/*
+** inventory_dtor.c for zappy in /home/damien/zappy/project/server/sources/inventory
+**
+** Made by damien sauvalle
+** Login   <sauval_d@epitech.net>
+**
+** Started on  Sun Jun 29 18:25:32 2014 damien sauvalle
+** Last update Sun Jun 29 18:27:45 2014 damien sauvalle
+*/
+
+#include	"inventory.h"
+
+void	inventory_dtor(__attribute__((unused))t_inventory *inventory)
+{
+
+}
