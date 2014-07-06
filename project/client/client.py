3c5d80479d4d41c695ed6cab521d27ac3909fc04
Barthelemy Delemotte <barthelemy.delemotte@epitech.eu>
2014-07-06 16:16:45 +0200

    client: program syntax subject compliant

--- project/client/client.py
+++ project/client/client.py
@@ -13,9 +13,9 @@ def disconnected_callback(data):
     print('client disconnected')
 
 def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('team_name', help='the name of the drone team')
-    parser.add_argument('-a', '--address', help='the server address or name', default='localhost')
+    parser = argparse.ArgumentParser(conflict_handler='resolve')
+    parser.add_argument('-n', '--team', help='the name of the drone team', required=True)
+    parser.add_argument('-h', '--hostname', help='the server address or name', default='localhost')
     parser.add_argument('-p', '--port', help='the port used by the server', type=int, default=4242)
     parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
     return parser.parse_args()
@@ -23,15 +23,16 @@ def parse_arguments():
 def main():
     args = parse_arguments()
 
-    print('Connecting on {0}:{1}'.format(args.address, args.port))
-    print('Team name =', args.team_name)
+    print('Connecting on {0}:{1}'.format(args.hostname, args.port))
+    print('Team name =', args.team)
     print('Verbose =', args.verbose)
 
     nw = client.network.Network()
-    nw.connect(args.address, args.port)
-    nw.hook_connected(connected_callback, (args.team_name, nw))
+    nw.hook_connected(connected_callback, (args.team, nw))
     nw.hook_disconnected(disconnected_callback, None)
+    nw.connect(args.hostname, args.port)
     nw.run()
 
 if __name__ == '__main__':
     main()
+    pass
