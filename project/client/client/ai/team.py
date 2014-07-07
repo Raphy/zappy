861c70fb72ac96439609796841cdb012d3dc72b1
Pelayo Parejo Pagador <pelayo.parejopagador@epitech.eu>
2014-07-07 19:20:03 +0000

    client: reteam v1

--- project/client/client/ai/team.py
+++ project/client/client/ai/team.py
@@ -2,9 +2,13 @@ class TeamInfo:
 
     def __init__(self):
         self.teammates = {}
+        self.messages = []
 
     def getDrone(self, drone_id):
         return self.teammates[drone_id] if self.teammates != {} else None
 
     def addDrone(self, drone_info):
         self.teammates[drone_info.id] = drone_info
+
+    def messages_with_type(self, type_):
+        return (message for message in self.messages if type_ == type(message))
