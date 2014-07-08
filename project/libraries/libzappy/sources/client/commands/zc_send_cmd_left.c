cbbbe8e4ebeadfde4b4b5dfeb6f15f06a4311c6b
Barthelemy Delemotte <barthelemy.delemotte@epitech.eu>
2014-07-08 23:12:20 +0200

    [libzappy] fix bug :p

--- project/libraries/libzappy/sources/client/commands/zc_send_cmd_left.c
+++ project/libraries/libzappy/sources/client/commands/zc_send_cmd_left.c
@@ -1,11 +1,11 @@
 /*
 ** zc_send_cmd_left.c for Zappy in /home/raphy/Epitech/Tech_2/zappy/project/libraries/libzappy/sources
-** 
+**
 ** Made by raphael defreitas
 ** Login   <defrei_r@epitech.net>
-** 
+**
 ** Started on  Sun Jun 29 10:07:53 2014 raphael defreitas
-** Last update Fri Jul  4 20:12:14 2014 raphael defreitas
+** Last update Tue Jul  8 23:11:27 2014 Barthelemy Delemotte
 */
 
 #include	<stdlib.h>
@@ -17,5 +17,5 @@ void		zc_send_cmd_left(t_zc *this)
 {
   if (this == NULL)
     return ;
-  zc_send(this, "avance\n");
+  zc_send(this, "gauche\n");
 }
