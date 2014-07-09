86d37844f42f63c3b504fb3b0d9cd8fd89e15b10
Raphael De Freitas <raphythegeek@gmail.com>
2014-07-09 18:41:39 +0200

    Separation into functions for the cmdd fptrs

--- project/libraries/libzappy/sources/client/zc_ctor_cmd_fptrs.c
+++ project/libraries/libzappy/sources/client/zc_ctor_cmd_fptrs.c
@@ -5,24 +5,45 @@
 ** Login   <defrei_r@epitech.net>
 ** 
 ** Started on  Tue Jul  1 01:53:30 2014 raphael defreitas
-** Last update Mon Jul  7 10:37:36 2014 raphael defreitas
+** Last update Wed Jul  9 18:41:10 2014 raphael defreitas
 */
 
 #include	"list.h"
 #include	"zappy.h"
 #include	"_zappy.h"
 
-int		zc_ctor_cmd_fptrs(t_zc *this)
+static int	player_cmd_fptrs(t_zc *this)
 {
-  if (list_enqueue(this->cmd_fptrs, &zc_cmd_welcome) == RET_FAILURE ||
-      list_enqueue(this->cmd_fptrs, &zc_ok) == RET_FAILURE ||
-      list_enqueue(this->cmd_fptrs, &zc_ko) == RET_FAILURE ||
-      list_enqueue(this->cmd_fptrs, &zc_cmd_msz) == RET_FAILURE ||
-      list_enqueue(this->cmd_fptrs, &zc_cmd_bct) == RET_FAILURE ||
-      list_enqueue(this->cmd_fptrs, &zc_rmt_authorized) == RET_FAILURE ||
+  if (list_enqueue(this->cmd_fptrs, &zc_ok) == RET_FAILURE ||
+      list_enqueue(this->cmd_fptrs, &zc_ko) == RET_FAILURE)
+    return (RET_FAILURE);
+  return (RET_FAILURE);
+}
+
+static int	graphic_cmd_fptrs(t_zc *this)
+{
+  if (list_enqueue(this->cmd_fptrs, &zc_cmd_msz) == RET_FAILURE ||
+      list_enqueue(this->cmd_fptrs, &zc_cmd_bct) == RET_FAILURE)
+    return (RET_FAILURE);
+  return (RET_SUCCESS);
+}
+
+static int	rmt_cmd_fptrs(t_zc *this)
+{
+  if (list_enqueue(this->cmd_fptrs, &zc_rmt_authorized) == RET_FAILURE ||
       list_enqueue(this->cmd_fptrs, &zc_rmt_unauthorized) == RET_FAILURE ||
       list_enqueue(this->cmd_fptrs, &zc_rmt_pubkey) == RET_FAILURE ||
       list_enqueue(this->cmd_fptrs, &zc_rmt_teams) == RET_FAILURE)
+    return (RET_FAILURE);
+  return (RET_SUCCESS);
+}
+
+int		zc_ctor_cmd_fptrs(t_zc *this)
+{
+  if (list_enqueue(this->cmd_fptrs, &zc_cmd_welcome) == RET_FAILURE ||
+      player_cmd_fptrs(this) == RET_FAILURE ||
+      graphic_cmd_fptrs(this) == RET_FAILURE ||
+      rmt_cmd_fptrs(this) == RET_FAILURE)
     return (RET_FAILURE);
   return (RET_SUCCESS);
 }
