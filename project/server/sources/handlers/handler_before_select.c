a090dce9795b4fea69b107ede8f1347a480e6bad
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-07-10 18:27:52 +0200

    correction £ --> e

--- project/server/sources/handlers/handler_before_select.c
+++ project/server/sources/handlers/handler_before_select.c
@@ -4,7 +4,7 @@
 #include          "tools_timespec.h"
 
 /*
-** set the serveur's timeout 
+** set the serveur's timeout
 ** t_events should be sort by the closest timeout
 */
 
@@ -13,16 +13,16 @@ void handler_before_select(t_zs *zs, void *data)
   t_server *server;
   t_timespec time;
   t_event *event;
-  
+
   server = (t_server*)data;
   clock_gettime(CLOCK_MONOTONIC, &time);
   printf("sec: %d, nano: %d\n", time.tv_sec, time.tv_nsec);
   if (data == NULL || server->events == NULL
-          || (event = list_front(serve£r->events)) == NULL)
+          || (event = list_front(server->events)) == NULL)
   {
     printf("No events! What should we do ?");
     zs_set_timeout(zs, 1, 0);
     return;
   }
-  zs_set_timeout(zs, event->end_time.tv_sec, event->end_time.tv_nsec);  
+  zs_set_timeout(zs, event->end_time.tv_sec, event->end_time.tv_nsec);
 }
