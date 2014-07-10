8e4ff777dd52c30fd1a7dc9acc197facc4191282
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-07-10 03:17:09 +0200

    good comment because We are Spartiat

--- project/libraries/libzappy/sources/server/commands/zs_cmd_sst.c
+++ project/libraries/libzappy/sources/server/commands/zs_cmd_sst.c
@@ -1,11 +1,11 @@
 /*
 ** zs_cmd_sst.c for Zappy in /home/raphy/Epitech/Tech_2/zappy/project/libraries/libzappy/sources/server/commands
-** 
+**
 ** Made by raphael defreitas
 ** Login   <defrei_r@epitech.net>
-** 
+**
 ** Started on  Sat Jul  5 15:48:50 2014 raphael defreitas
-** Last update Thu Jul 10 02:50:18 2014 raphael defreitas
+** Last update Thu Jul 10 03:15:46 2014 damien sauvalle
 */
 
 #include	<stdbool.h>
@@ -22,7 +22,7 @@ bool		zs_cmd_sst(t_zs *this, t_zc *zc, const char *cmd)
     return (false);
   if (!my_str_is_numeric(cmd + 4))
     {
-      // ToDo SBP
+      /* ToDo SBP*/
       return (true);
     }
   time = strtoul(cmd + 4, NULL, 0);
