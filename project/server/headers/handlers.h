41a9cd76c6c4684df74b0bffa1cc28889afa7d9a
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-07-11 17:57:47 +0200

    create player + void * t_server

--- project/server/headers/handlers.h
+++ project/server/headers/handlers.h
@@ -5,7 +5,7 @@
 ** Login   <defrei_r@epitech.net>
 **
 ** Started on  Fri Jun 27 19:39:18 2014 raphael defreitas
-** Last update Mon Jul  7 10:14:36 2014 raphael defreitas
+** Last update Fri Jul 11 17:49:31 2014 damien sauvalle
 */
 
 #ifndef		HANDLERS_H_
@@ -23,7 +23,7 @@ void		set_server_handlers(t_zs *, t_server *);
 void		errno_handler(t_zs *, int, const char *, void *);
 void		callback_handler(t_zs *, t_zht, void *);
 void		timeout_handler(t_zs *, void *);
-void		client_connected_handler(t_zs *, t_zc *, void *);
+void		client_connected_handler(t_zs *, t_zc *, t_server *);
 void		client_disconnected_handler(t_zs *, t_zc *, void *);
 
 /*
@@ -32,7 +32,7 @@ void		client_disconnected_handler(t_zs *, t_zc *, void *);
 void		set_client_handlers(t_zc *, void *);
 void		cmd_unknown_handler(t_zs *, t_zc *, const char *, void *);
 
-void		cmd_team_name_handler(t_zs *, t_zc *, const char *, void *);
+void		cmd_team_name_handler(t_zs *, t_zc *, const char *, t_server *);
 
 void		cmd_graphic_handler(t_zs *, t_zc *, void *);
 
