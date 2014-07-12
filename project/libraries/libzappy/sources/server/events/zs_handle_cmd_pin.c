10ea58549233339f2b5ecf291062bf6b94d6a847
Raphael De Freitas <raphythegeek@gmail.com>
2014-07-12 17:32:31 +0200

    [libzappy] Je suis un boulet v42

--- project/libraries/libzappy/sources/server/events/zs_handle_cmd_pin.c
+++ project/libraries/libzappy/sources/server/events/zs_handle_cmd_pin.c
@@ -5,7 +5,7 @@
 ** Login   <defrei_r@epitech.net>
 ** 
 ** Started on  Thu Jun 26 11:36:48 2014 raphael defreitas
-** Last update Sat Jul 12 15:40:30 2014 raphael defreitas
+** Last update Sat Jul 12 17:31:29 2014 raphael defreitas
 */
 
 #include	"zappy.h"
@@ -15,7 +15,7 @@ void		zs_handle_cmd_pin(t_zs *this, t_zc *zc, unsigned int uid)
 {
   t_zh		zh;
 
-  zh = this->hooks[ZHT_CMD_PIN];
+  zh = zc->hooks[ZHT_CMD_PIN];
   if (zh.handler)
     zh.handler(this, zc, uid, zh.data);
   zs_handle_callback(this, ZHT_CMD_PIN);
