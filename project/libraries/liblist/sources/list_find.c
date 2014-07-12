e03d0d57f901bee99f7bd6ca2fc6563973aaec3d
Raphael De Freitas <raphythegeek@gmail.com>
2014-07-12 13:17:54 +0200

    finder explanation

--- project/libraries/liblist/sources/list_find.c
+++ project/libraries/liblist/sources/list_find.c
@@ -5,14 +5,15 @@
 ** Login   <defrei_r@epitech.net>
 ** 
 ** Started on  Thu Apr 17 20:42:12 2014 raphael defreitas
-** Last update Sun Jun 29 06:28:15 2014 raphael defreitas
+** Last update Sat Jul 12 13:15:26 2014 raphael defreitas
 */
 
+#include	<stdbool.h>
 #include	<stdlib.h>
 
 #include	"list.h"
 
-void		*list_find(t_list *this, int (*find_fptr)(), const void *srch,
+void		*list_find(t_list *this, bool (*finder)(), const void *searched,
 			   t_it_type type)
 {
   t_iterator	iterator;
@@ -24,7 +25,7 @@ void		*list_find(t_list *this, int (*find_fptr)(), const void *srch,
   while ((item = iterator_current(&iterator)))
     {
       iterator_next(&iterator);
-      if (find_fptr(item_data(item), srch) == 0)
+      if (finder(item_data(item), srch))
 	return (type == IT_ITEM ? item : item_data(item));
     }
   return (NULL);
