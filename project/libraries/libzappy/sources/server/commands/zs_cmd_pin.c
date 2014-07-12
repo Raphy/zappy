0d8ea65a090c56ec73232b2b9e565ec6ba9f268f
Raphael De Freitas <raphythegeek@gmail.com>
2014-07-12 23:31:21 +0200

    test

--- project/libraries/libzappy/sources/server/commands/zs_cmd_pin.c
+++ project/libraries/libzappy/sources/server/commands/zs_cmd_pin.c
@@ -5,11 +5,12 @@
 ** Login   <defrei_r@epitech.net>
 **
 ** Started on  Sat Jul  5 15:48:50 2014 raphael defreitas
-** Last update Sat Jul 12 18:14:51 2014 raphael defreitas
+** Last update Sat Jul 12 23:28:10 2014 raphael defreitas
 */
 
 #include	<stdbool.h>
 #include	<stdlib.h>
+#include	<string.h>
 
 #include	"my.h"
 #include	"zappy.h"
@@ -19,10 +20,14 @@ bool		zs_cmd_pin(t_zs *this, t_zc *zc, const char *cmd)
 {
   unsigned int	uid;
 
-  if (!my_match(cmd, "pin *"))
+  printf("pin: %s (%d)\n", cmd, strncmp(cmd, "pin", 3));
+  if (strncmp(cmd, "pin", 3) != 0)
     return (false);
-  if (!my_str_is_numeric(cmd + 4))
+  if (!my_match(cmd, "pin *") ||
+      !my_str_is_numeric(cmd + 4))
     {
+      printf("sbp par pin (%s) (%s)\n", my_match(cmd, "pin *") ? "match" : "no match",
+	     my_str_is_numeric(cmd + 4) ? "numeric" : "no numeric");
       zs_send_cmd_sbp(this, zc);
       return (true);
     }
