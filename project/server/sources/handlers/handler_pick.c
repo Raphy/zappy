2e865f0cc59adb805645df2f6312cdd070d50b66
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-07-13 14:57:45 +0200

    handler pick

--- project/server/sources/handlers/handler_pick.c
+++ project/server/sources/handlers/handler_pick.c
@@ -1,11 +1,22 @@
-#include "zappy.h"
-#include  "player.h"
-#include "bundle.h"
-#include "event.h"
+/*
+** handler_pick.c for zappy in /home/damien/zappy/project/server/sources/handlers
+**
+** Made by damien sauvalle
+** Login   <sauval_d@epitech.net>
+**
+** Started on  Sun Jul 13 11:42:41 2014 damien sauvalle
+** Last update Sun Jul 13 14:46:28 2014 damien sauvalle
+*/
+
+#include	<string.h>
+
+#include	"zappy.h"
+#include	"player.h"
+#include	"bundle.h"
+#include	"event.h"
 #include	"player_action.h"
 
-void          handler_pick(__attribute__((unused))t_zs *zs,
-        __attribute__((unused))t_zc *zc, void *data)
+void          handler_pick(t_zs *zs, t_zc *zc, const char *str, void *data)
 {
   t_scratch_event bundle;
 
@@ -13,5 +24,6 @@ void          handler_pick(__attribute__((unused))t_zs *zs,
     return ;
   bundle.action = &player_action_pick;
   bundle.cycle = 7;
+  ((t_bundle*)data)->str = strdup(str);
   event_create_from_scratch(&bundle, (t_bundle*)data);
 }
