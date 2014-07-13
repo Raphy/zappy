3a812aa02a7621253d96e4f17a46969c15ae5187
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-07-13 14:58:15 +0200

    pick

--- project/server/sources/player/player_action_pick.c
+++ project/server/sources/player/player_action_pick.c
@@ -5,13 +5,18 @@
 ** Login   <sauval_d@epitech.net>
 **
 ** Started on  Sat Jul  5 17:49:12 2014 damien sauvalle
-** Last update Sat Jul  5 17:49:21 2014 damien sauvalle
+** Last update Sun Jul 13 12:10:36 2014 damien sauvalle
 */
+
+#include	<stdio.h>
 
 #include	"player.h"
 
-void player_action_pick(t_player *player, void *data)
+void	player_action_pick(t_player *player, void *data, char *c)
 {
-	(void)player;
-	(void)data;
+  (void)player;
+  (void)data;
+
+  printf("(############)---> YOLO SWAGG : %s\n", c);
+
 }
